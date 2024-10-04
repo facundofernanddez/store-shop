90cd7ec3215c0853431a5851cb7bb7b4436e0dc6
Alejandro Facundo Fernandez <91287409+facundofernanddez@users.noreply.github.com>
2024-10-04 10:00:05 -0300

    create grid

--- app/components/OneProductCard.tsx
+++ app/components/OneProductCard.tsx
@@ -4,18 +4,20 @@ import { IProducts } from "../interface/products";
 export default function OneProductCard({ product }: { product: IProducts }) {
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="max-w-sm rounded overflow-hidden shadow-lg">
-        <Image
-          className="w-full h-56"
-          width={24}
-          height={24}
-          src={product.image}
-          alt={product.title}
-        />
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">{product.title}</div>
-          <p className="text-gray-700 text-base">{product.description}</p>
-          <p className="text-gray-500 text-sm">Price: ${product.price}</p>
+      <div className="max-w-xl rounded overflow-hidden shadow-lg">
+        <div className="grid grid-cols-2">
+          <Image
+            className="w-full p-2 flex items-center justify-center"
+            width={24}
+            height={24}
+            src={product.image}
+            alt={product.title}
+          />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">{product.title}</div>
+            <p className="text-gray-700 text-base">{product.description}</p>
+            <p className="text-gray-500 text-sm">Price: ${product.price}</p>
+          </div>
         </div>
       </div>
     </div>
